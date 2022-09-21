793471cf6c2b47e55511c6f58c14870f3fa3c4d0
victor ortega <vorteg.r@gmail.com>
2022-09-21 13:28:03 -0500

    actualizando perfil

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import IconLink from "./IconLink";
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
 
 function PortfolioBlock(props) {
-   const {image, live, source, title} = props;
+   const { image, live, source, title } = props;
    return (
       <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
-         <Box component={'img'} src={image} alt={'mockup'}/>
-         <h1 style={{fontSize: '2rem'}}>{title}</h1>
+         <Box minWidth={20} maxHeight={380} component={'img'} src={image} alt={'mockup'} />
+         <h1 style={{ fontSize: '2rem' }}>{title}</h1>
          <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'}
-              alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
+            alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
             <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-               <IconLink link={live} title={'Live Demo'} icon={'fa fa-safari'}/>
+               <IconLink link={live} title={'Live Demo'} icon={'fa fa-safari'} />
             </Box>
             <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-               <IconLink link={source} title={'Source Code'} icon={'fa fa-code'}/>
+               <IconLink link={source} title={'Source Code'} icon={'fa fa-code'} />
             </Box>
          </Box>
       </Box>
--- src/info/Info.js
+++ src/info/Info.js
@@ -1,9 +1,6 @@
-import self from "../img/self.png"
+import self from "../img/self.jpeg"
 import mock1 from "../img/mock1.png"
-import mock2 from "../img/mock2.png"
-import mock3 from "../img/mock3.png"
-import mock4 from "../img/mock4.png"
-import mock5 from "../img/mock5.png"
+import mock2 from "../img/mock2.jpeg"
 
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
@@ -23,10 +20,10 @@ of what each of the values mean.
  */
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
-    position: "a Full Stack Developer",
+    firstName: "Victor",
+    lastName: "Ortega",
+    initials: ".py", // the example uses first and last, but feel free to use three or more if you like.
+    position: "Software Apps Developer & Entrepreneur",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
     baseColor: colors[0],
@@ -37,23 +34,15 @@ export const info = {
         },
         {
             emoji: '🌎',
-            text: 'based in the US'
+            text: 'based in Mexico'
         },
         {
             emoji: "💼",
-            text: "Systems Engineer at Google"
-        },
-        {
-            emoji: "📧",
-            text: "johnsmith@gmail.com"
+            text: "Strategist - Custom Software Solutions at Pichones Corp"
         }
     ],
     socials: [
-        {
-            link: "https://facebook.com",
-            icon: 'fa fa-facebook',
-            label: 'facebook'
-        },
+
         {
             link: "https://instagram.com",
             icon: 'fa fa-instagram',
@@ -74,67 +63,46 @@ export const info = {
             icon: "fa fa-twitter",
             label: 'twitter'
         }
-// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-// Just change the links so that they lead to your social profiles.
+        // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
+        // Just change the links so that they lead to your social profiles.
 
     ],
-    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    bio: "Hello! I'm Victor. Engineer with 13 years of experience in electronic and software areas, specialist on software architecture analysis and design. Very passionate for development and building projects from scratch. In recent years been working in developer roles for Computer Vision (AI/ML), Linux OS customized, automated testing, automated process, developing IoT applications, cloud applications deployment, creating documentation and web development.  ",
     skills:
-        {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
-            exposedTo: ['nodejs', 'python', 'adobe illustrator']
-        }
+    {
+        proficientWith: ['Technical Lead Python Developer & Computer Vision(AI / ML)', ' Senior Python developer', 'Full Stack developer', 'Software Engineer Developer lead', 'System Validation engineer', 'Program Manager Intern', 'Hardware Technician'],
+        exposedTo: ['python', 'django', 'flask', 'fastapi', 'pandas', 'jupyter notebook', 'computer vision', 'deep learning', 'pytorch', 'react', 'nextjs', 'nodejs', 'javascript', 'typescript', ' and more']
+    }
     ,
     hobbies: [
+
         {
-            label: 'reading',
-            emoji: '📖'
-        },
-        {
-            label: 'theater',
-            emoji: '🎭'
+            label: 'cats',
+            emoji: '🐱'
         },
         {
             label: 'movies',
             emoji: '🎥'
         },
         {
-            label: 'cooking',
-            emoji: '🌶'
+            label: 'video games',
+            emoji: '🎮'
         }
-// Same as above, change the emojis to match / relate to your hobbies or interests.
-// You can also remove the emojis if you'd like, I just think they look cute :P
+        // Same as above, change the emojis to match / relate to your hobbies or interests.
+        // You can also remove the emojis if you'd like, I just think they look cute :P
     ],
     portfolio: [ // This is where your portfolio projects will be detailed
         {
-            title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
+            title: "Pichones Corp",
+            live: "https://pichonescorp.com/", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/vorteg/pichonGatsby", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock1
         },
         {
-            title: "Project 2",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            title: "PC English",
+            live: "https://www.facebook.com/PCenglishclass",
+            source: "",
             image: mock2
-        },
-        {
-            title: "Project 3",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock3
-        },
-        {
-            title: "Project 4",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock4
-        },
-        {
-            title: "Project 5",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock5
         }
     ]
 }
